1e3617ee8897cb523a40a4d3e5300a2403db1c89
Rothri <fortheloveofgod@localhost.localdomain>
2017-07-02 08:42:47 -0500

    Euler3!$

---
+++ Euler3/Euler3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long prime = 1;
+            long subject = 600851475143;
+            int loopcounter = 0;
+            while (subject > 1)
+            {
+                prime++;
+                if (subject % prime == 0)
+                {
+                    Console.WriteLine(prime);
+                    subject = subject / prime;
+                }
+                if (prime > 3000000000)
+                {
+                    prime = 0;
+                    loopcounter += 1;
+                }
+                if (loopcounter > 3)
+                {
+                    Console.WriteLine("Failure");
+                    break;
+                }
+            }
+           
+        }
+    }
+}
