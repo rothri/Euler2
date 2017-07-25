f197a645c5d825b3d4dd12c2fba9688869f131a3
rothri <montresor35@gmail.com>
2017-07-25 07:10:38 -0500

    Euler6!

---
+++ Euler6/Euler6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumSquares = 0;
+            int square = (100 * 100 + 100) / 2;
+            //Thank you Calc2
+            square *= square;
+            int i = 1;
+            while (i < 101)
+            {
+                sumSquares += i * i;
+                i++;
+            }
+            Console.WriteLine(square + " - " + sumSquares + " = " +(square - sumSquares));
+        }
+    }
+}
